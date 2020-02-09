35269bfe18a8b5c9f49cade3c9a31ef13546201f
Gauravjain449 <gauravjain449@gmail.com>
2020-02-09 22:48:30 +0530

    local commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hi there!/i);
+  const linkElement = getByText(/You there!/i);
   expect(linkElement).toBeInTheDocument();
 });
